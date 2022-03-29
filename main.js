b562cc30ad67317adf56a3eff9ce89b5dd4eceeb
DimitriHinds <81481893+DimitriHinds@users.noreply.github.com>
2022-03-29 17:44:46 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -8,18 +8,15 @@ function setup(){
     video.size(300,290);
     video.hide();
 }
-
 function start(){
     object_Detector = ml5.objectDetector('cocossd',modelLoaded);
     document.getElementById("status").innerHTML = "Status: Detecting Objects";
     input_text = document.getElementById("input_id").value;
 }
-
 function modelLoaded(){
     console.log("Model_Loaded");
     Status = true;
 }
-
 function draw(){
     image(video,0,0,300,290);
 }
